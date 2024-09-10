346388721f93e24645eb2c307505112dcca2bac5
Ranjith Babu <ranjithbabu0912@gmail.com>
2024-09-10 20:25:49 +0530

    changes make

--- src/main.jsx
+++ src/main.jsx
@@ -3,30 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './common/ThemeContext.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import About from './sections/About/About.jsx'
-import Hero from './sections/Hero/Hero.jsx'
-
-const router =  createBrowserRouter([
-  {
-    path: "/portfolio/",
-    element: <App />,
-    children: [{
-      path: "/portfolio/",
-      element: <Hero />,
-    },
-    {
-      path: "/portfolio/About",
-      element: <About/>,
-    },
-  ],
-  },
-]);
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    
     <ThemeProvider>
       <App />
     </ThemeProvider>
